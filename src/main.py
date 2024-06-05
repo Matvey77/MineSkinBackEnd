4704fff8b8e3dfa029a37f5699d890128339a2dc
AndrewShuvaev <andrew.shuw@gmail.com>
2024-06-05 23:44:22 +1000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch andrey
    Your branch is up to date with 'origin/andrey'.
    
    Changes to be committed:
    	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -4,21 +4,28 @@ from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
 
 from operations.router import router as router_operation
+from news.router import router as router_news
 
 app = FastAPI(
     title="Trading App"
 )
 
+# Маршруты аутентификации
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["Auth"],
 )
 
+# Маршруты регистрации пользователей
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["Auth"],
 )
 
+# Маршруты операций
 app.include_router(router_operation)
+
+# Маршруты новостей
+app.include_router(router_news)
