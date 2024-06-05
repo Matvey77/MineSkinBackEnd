9983cd6c3ef93b13416ff401763afaac79f68e04
AndrewShuvaev <andrew.shuw@gmail.com>
2024-06-05 23:57:56 +1000

    commit
    переписаны роуты под бд

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
-
 from operations.router import router as router_operation
 from news.router import router as router_news
 
@@ -10,22 +9,21 @@ app = FastAPI(
     title="Trading App"
 )
 
-# Маршруты аутентификации
+# Аутентификация и регистрация
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["Auth"],
 )
 
-# Маршруты регистрации пользователей
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["Auth"],
 )
 
-# Маршруты операций
+# Операции (существующие)
 app.include_router(router_operation)
 
-# Маршруты новостей
-app.include_router(router_news)
+# Новости
+app.include_router(router_news, prefix="/news", tags=["News"])
